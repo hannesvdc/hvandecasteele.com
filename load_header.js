ba960fe2191e24e46f097ffab5d1f9a2dc28f745
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-24 15:09:47 +0100

    Moved header loader to separate file.

---
+++ load_header.js
@@ -0,0 +1,9 @@
+// Load the header HTML dynamically
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header_nav.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading header:', error));
+  });
