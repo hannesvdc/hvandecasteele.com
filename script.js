c473774127636a1f3c6e379d30dd074c87d79e13
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-24 15:01:17 +0100

    Changed images to fixed size.

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
       title: "PyCont",
       link: "projects/pycont.html",
       description: "Numerical Continuation and Bifurcation Detection Tool written in Python.",
-      image: "images/pycont.png"
+      image: "images/pycont_resized.png"
     },
     {
       title: "Backmapping for Protein Folding",
       link: "projects/backmapping.html",
       description: "Micro-macro Markov chain Monte Carlo sampling with backmapping for protein folding.",
-      image: "images/backmapping.png"
+      image: "images/backmapping_resized.png"
     },
     {
       title: "Locating Saddle Points on Manifolds",
       link: "projects/saddle.html",
       description: "Locating Saddle Points on Manifolds defined by Point Clouds using Gentlest Ascent Dynamics and Gradient Extremals.",
-      image: "images/saddle.png"
+      image: "images/saddle_resized.png"
     },
     {
       title: "Micro-Macro MCMC Sampling",
       link: "projects/mM_MCMC.html",
       description: "Micro-Macro Markov Chain Monte Carlo Accelerated Sampling for Molecular Dynamics using Reaction Coordinates.",
-      image: "images/mMFigure_png.png"
+      image: "images/mMFigure_png_resized.png"
     },
     {
       title: "Micro-Macro Acceleration",
       link: "project/micro_macro_acc.html",
       description: "Micro-macro acceleration for stochastic differential equations with a time-scale separation.",
-      image: "images/micro_macro_acc.png"
+      image: "images/micro_macro_acc_resized.png"
     },
   ];
 
