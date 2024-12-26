66e959845f09f928e6139667ce5d1ae6b9788490
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-26 18:01:31 +0100

    putting all fiels in one directory

--- load_header.js
+++ load_header.js
@@ -1,22 +1,10 @@
-const scriptElement = document.currentScript; // Capture the script element
-console.log('scriptElement', scriptElement);
 
 // Load the header HTML dynamically
 document.addEventListener("DOMContentLoaded", function() {
-  console.log('Header directory: ', scriptElement.src);
-  if (scriptElement) {
-    const scriptPath = scriptElement.src; // Get the full path of the current script
-    const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1); // Extract the base directory
-    const headerPath = basePath + 'header_nav.html'; // Append the header file name
-    console.log('HeaderPath: ', headerPath);
-
-    fetch(headerPath)
+    fetch('header_nav.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('header-container').innerHTML = data;
       })
       .catch(error => console.error('Error loading header:', error));
-  } else {
-    console.error("Error: Unable to determine the script path.");
-  }
 });
--- script.js
+++ script.js
@@ -2,31 +2,31 @@ document.addEventListener("DOMContentLoaded", () => {
   const projects = [
     {
       title: "PyCont",
-      link: "projects/pycont.html",
+      link: "pycont.html",
       description: "Numerical Continuation and Bifurcation Detection Tool written in Python.",
       image: "images/pycont_resized.png"
     },
     {
       title: "Backmapping for Protein Folding",
-      link: "projects/backmapping.html",
+      link: "backmapping.html",
       description: "Micro-macro Markov chain Monte Carlo sampling with backmapping for protein folding.",
       image: "images/backmapping_resized.png"
     },
     {
       title: "Locating Saddle Points on Manifolds",
-      link: "projects/saddle.html",
+      link: "saddle.html",
       description: "Locating Saddle Points on Manifolds defined by Point Clouds using Gentlest Ascent Dynamics and Gradient Extremals.",
       image: "images/saddle_resized.png"
     },
     {
       title: "Micro-Macro MCMC Sampling",
-      link: "projects/mM_MCMC.html",
+      link: "mM_MCMC.html",
       description: "Micro-Macro Markov Chain Monte Carlo Accelerated Sampling for Molecular Dynamics using Reaction Coordinates.",
       image: "images/mMFigure_png_resized.png"
     },
     {
       title: "Micro-Macro Acceleration",
-      link: "projects/micro_macro_acc.html",
+      link: "micro_macro_acc.html",
       description: "Micro-macro acceleration for stochastic differential equations with a time-scale separation.",
       image: "images/micro_macro_acc_resized.png"
     },
