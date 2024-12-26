012f5e7c2059ca51f34754667a411af8e6256d53
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-26 13:35:36 +0100

    debugging

--- load_header.js
+++ load_header.js
@@ -1,5 +1,8 @@
 // Load the header HTML dynamically
 document.addEventListener("DOMContentLoaded", function() {    
+  const scriptPath = document.currentScript.src; // Path to load_header.js
+  console.log('scriptpath', scriptPath);
+  
     fetch('/header_nav.html')
       .then(response => response.text())
       .then(data => {
