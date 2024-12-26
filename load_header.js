c3102ce866a71d7fbecffcbea32e8f14e3a47f35
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-26 12:16:08 +0100

    Adding my own debugging info.

--- load_header.js
+++ load_header.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const scriptPath = document.currentScript.src; // Path to load_header.js
     const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1);
     const headerPath = basePath + 'header_nav.html';
+    console.log(headerPath);
     
     fetch(headerPath)
       .then(response => response.text())
