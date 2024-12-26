9dd12a76a6db20fce0b162f0f46d0dcc0abaa124
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-26 12:20:56 +0100

    Import error still not fixex/

--- load_header.js
+++ load_header.js
@@ -1,11 +1,6 @@
 // Load the header HTML dynamically
-document.addEventListener("DOMContentLoaded", function() {
-    const scriptPath = document.currentScript.src; // Path to load_header.js
-    const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1);
-    const headerPath = basePath + 'header_nav.html';
-    console.log(headerPath);
-    
-    fetch(headerPath)
+document.addEventListener("DOMContentLoaded", function() {    
+    fetch('/header_nav.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('header-container').innerHTML = data;
