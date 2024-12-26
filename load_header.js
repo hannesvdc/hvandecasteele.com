fd71f54f33ebaff5726722c211e960623c16d9d9
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-26 11:46:57 +0100

    fixing cwd error.

--- load_header.js
+++ load_header.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log('scripts:', scripts);
     console.log('src:', src);
     
-    fetch('/header_nav.html')
+    fetch('header_nav.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('header-container').innerHTML = data;
