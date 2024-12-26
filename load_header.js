24d369afd621e606366c0b6898bfa177ec8cb1e9
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-26 12:02:00 +0100

    Fixing debugging infro.

--- load_header.js
+++ load_header.js
@@ -2,8 +2,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var header_nav = document.getElementsByTagName("header_nav")
     var header_nav_src = scripts[scripts.length-1].src;
-    console.log('scripts:', header_nav);
-    console.log('src:', header_nav_src);
+    console.log('header_nav:', header_nav);
+    console.log('header_nva_src:', header_nav_src);
     
     fetch(header_nav_src)
       .then(response => response.text())
