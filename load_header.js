d8bc9e9f0d7749685490a7f5c1b0a13296498d3e
Hannes Vandecasteele <hannesvdc@gmail.com>
2024-12-27 09:12:26 +0100

    Updated front text.

--- load_header.js
+++ load_header.js
@@ -1,19 +1,18 @@
 const scriptElement = document.currentScript;
 
-// Load the header HTML dynamically
 document.addEventListener("DOMContentLoaded", function() {
   if (scriptElement) {
-    const scriptPath = scriptElement.src; // Get the full path of the current script
-    const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1); // Extract the base directory
-    const headerPath = basePath + 'header_nav.html'; // Append the header file name
+      const scriptPath = scriptElement.src;
+      const basePath = scriptPath.substring(0, scriptPath.lastIndexOf('/') + 1);
+      const headerPath = basePath + 'header_nav.html';
 
-    fetch(headerPath)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('header-container').innerHTML = data;
-      })
-      .catch(error => console.error('Error loading header:', error));
-    } else{
+      fetch(headerPath)
+          .then(response => response.text())
+          .then(data => {
+              document.getElementById('header-container').innerHTML = data;
+          })
+          .catch(error => console.error('Error loading header:', error));
+  } else{
       console.error("Error: Unable to determine the script path.");
-    }
+  }
 });
